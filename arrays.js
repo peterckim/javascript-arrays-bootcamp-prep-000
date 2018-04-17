9558ad7c620ba7f2da69bb7f39888be11b276aad
Peter Kim <peterckim96@gmail.com>
2018-04-17 15:17:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,6 +35,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
+  array = array.slice(1);
   return array;
 }
