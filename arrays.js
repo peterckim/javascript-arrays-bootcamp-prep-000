1ba8b48444c31d6131b777a4bc735bf133b5d9ef
Peter Kim <peterckim96@gmail.com>
2018-04-17 15:15:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,9 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
