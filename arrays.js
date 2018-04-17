8bebba533469317436cba896225b30ecd5de9764
Peter Kim <peterckim96@gmail.com>
2018-04-17 15:18:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,11 +40,11 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.shift();
+  array.pop();
   return array;
 }
 
 function removeElementFromEndOfArray(array) {
-  array = array.slice(1);
+  array = array.slice(0, array.length - 1);
   return array;
 }
